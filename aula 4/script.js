988e01249cf58ccf946997b58170e47d7f04a04c
Saymon Gabriel <saymonzinho117@gmail.com>
2025-11-24 19:51:14 -0300

    merge

--- aula 4/script.js
+++ aula 4/script.js
@@ -1,18 +1,20 @@
-add();
-
-function add(){
+function add() {
     var lista = document.getElementById('itens');
-
     var campo = document.getElementById('campo');
+
+    if (campo.value.trim() === "") return;  
 
     var li = document.createElement('li');
 
-    li.innerHTML = campo.value;
     
+    li.innerHTML = `${campo.value} <button onclick="apagar(this)"><img src="./Imagens/lixeira.png" alt="Excluir" style="width: 16px; height: 16px;"></button>`;
+
     lista.appendChild(li);
 
     campo.value = "";
+}
 
-    var div = document.createElement('div');
-    
+
+function apagar(botao) {
+    botao.parentElement.remove();
 }
