2ef2022498d512d05133ea2a0ad50576a54b4e62
Saymon Gabriel <saymonzinho117@gmail.com>
2025-11-24 21:46:03 -0300

    merge

--- aula 6/script.js
+++ aula 6/script.js
@@ -7,3 +7,19 @@ function salvar(){
 
     localStorage.setItem('nome',nome);
 }
+
+function excluir(){
+    localStorage.removeItem('nome');
+    input.value ="";
+}
+const pessoas = ["Guilherme","Maria","Jose","Darlana","Saymon","Ariel"];
+
+function mostra{
+    console.log = (pessoas);
+
+    for(var i = 0; i <= 5; i++){
+        const h1 = document.createElement("h1");
+        h1.innerHTML = pessoas[i];
+        document.body.appendChild(h1);
+    }
+}
